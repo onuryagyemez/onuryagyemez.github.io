e54a91e50821a55fd7795cbf286d3e6775f5467d
onuryagyemez <onuryagyemez@gmail.com>
2022-09-30 00:15:12 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider3').slick({
+        dots: false,
+        infinite: false,
+        centerMode: false,
+        vertical: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        arrows: false,
+});
+});
